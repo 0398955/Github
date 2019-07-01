f960187158bc30412f40877a3e51a3c95697825e
0398955 <34764115+0398955@users.noreply.github.com>
2019-07-01 11:53:32 +0300

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+wd stop
+
+miner stop
+
+#autoswitch stop
+
+/hive/miners/phoenixminer/4.2c/ ./PhoenixMiner -pool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -wal 3QhqCB7Hcj6QMJKMKWVBQBq8tRXVVBuneP -pass x -proto  4 -stales 0
